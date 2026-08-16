f327523086234bde3c8d966c0337ea1f4063a439
Ashif Eqbal <ashif8984@gmail.com>
2026-08-16 21:30:13 +0530

    Added a health route

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
             <meta charset="UTF-8">
             <title>Express HTML Demo</title>
             <style>
-                body { background-color: #c23838; font-family: sans-serif; text-align: center; margin-top: 50px; }
+                body { background-color: #e3d9d9; font-family: sans-serif; text-align: center; margin-top: 50px; }
                 h1 { color: #333; }
                 h2 { color: #030a04; }
                 p { color: #330303; }
@@ -18,12 +18,17 @@ app.get('/', (req, res) => {
         </head>
         <body>
             <h1>Hello from Vande Bharat!</h1>
-            <h2>Welcome to the Express HTML Demo</h2>
+            <h2>Welcome to the Indian Railway</h2>
             <p>This web application is deployed using Kubernetes and Argocd</p>
         </body>
         </html>
     `);
 });
+
+app.get('/health', (req, res) => {
+    res.status(200).send('OK');
+});
+
 app.listen(port, () => {
   console.log(`App running on http://localhost:${port}`);
 });
